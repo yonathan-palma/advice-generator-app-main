5b8258ddaa1b2ad1dfc1a551e718b2bb93809297
yonathan <yonathanpalmarey@gmail.com>
2022-07-20 01:02:03 -0400

    animation

--- src/main.js
+++ src/main.js
@@ -2,16 +2,22 @@ import advice_search from "./utils/getData.js";
 
 const API = "https://api.adviceslip.com/advice";
 const icon_advice = document.getElementById("icon_advice");
+const parrafo = document.querySelector("#card_body p");
+const titleId = document.getElementById("id_advice");
 
 async function addAdviceCard(){
+    parrafo.innerHTML = "<h2>LOADING.</h2>";
+
     const data = await advice_search(API);
-    const titleId = document.getElementById("id_advice");
-    const parrafo = document.querySelector("#card_body p");
 
     titleId.textContent = data.slip.id;
-    parrafo.textContent = data.slip.advice
+    parrafo.textContent = data.slip.advice;
+    parrafo.style.animation = "2s anim-lineUp ease-out forwards";
 }
 
 icon_advice.addEventListener("click", addAdviceCard);
+parrafo.addEventListener("animationend", ()=>{
+    parrafo.style.animation = "none";
+});
 
 
